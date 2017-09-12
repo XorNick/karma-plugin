b0a7b7bc7b9260ba6ea0301ab40b9ca716c31d64
XorNick <redducknick@gmail.com>
2017-09-12 04:41:11 -0400

    Initial commit

---
+++ src/main/java/net/xornick/karmasystem/KarmaPlugin.java
@@ -0,0 +1,11 @@
+package net.xornick.karmasystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KarmaPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
